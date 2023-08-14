ee2f9c5e06ea865fc7b6fa9ebc6162c202afe807
NelsonFrank <nelsonfr78@outlook.com>
2023-08-14 14:24:02 +0300

    fix: Remove edge config in message route

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -4,9 +4,6 @@ if (!process.env.OPENAI_API_KEY) {
     throw new Error("Missing env var from OpenAI");
 }
 
-export const config = {
-    runtime: "edge",
-};
 
 export async function POST(req: Request): Promise<Response> {
     const { prompt } = (await req.json()) as {
